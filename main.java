64648685c2c930f5de7c9ddfc938a98aa4c15625
samarth272 <samarthadkitte@gmail.com>
2024-01-18 13:36:06 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class main{
+
+    public static void main(String[]args){
+
+        System.out.println("This is java program");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+    }
+}
